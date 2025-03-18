00ca6389e0baf244ac4da47e3fe13396d95e7b07
Suar <albertosuar2004@gmail.com>
2025-03-18 20:45:34 +0100

    Aggiunto file cadenza.h

---
+++ Logic/Headers/cadenza.h
@@ -0,0 +1,14 @@
+#ifndef CADENZA_H
+#define CADENZA_H
+
+enum class Cadenza {
+    Undefined,
+    Quotidiano,
+    Settimanale,
+    Mensile,
+    Trimestrale,
+    Semestrale,
+    Annuale
+};
+
+#endif CADENZA_H
