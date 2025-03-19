9fb2a44a1a54dde8e1ccf78fbd7fc4b4c626ba27
Suar <albertosuar2004@gmail.com>
2025-03-19 08:47:39 +0100

    Modificato film.h

--- Logic/Headers/film.h
+++ Logic/Headers/film.h
@@ -15,11 +15,23 @@ protected:
     };
 private:
     string regista;
+    string composer;
+    string casaProduttrice;
     Genere genere;
 public:
     Film(string n = "", unsigned int a = 0, Lingua l = Lingua::Undefined,
          Risoluzione r = Risoluzione::Undefined, unsigned int d = 0,
-         Genere g = Genere::Undefined);
+         string re = "", string co = "", string cp = "", Genere g = Genere::Undefined);
+
+    string getRegista() const;
+    string getComposer() const;
+    string getCasa() const;
+    string getGenere() const;
+
+    void setRegista(const string& re);
+    void setComposer(const string& co);
+    void setCasa(const string& ca);
+    void setGenere(const Genere& g);
 };
 
 #endif FILM_H
