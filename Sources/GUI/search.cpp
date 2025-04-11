c7673cf7fd46e68fb5b7e145a5223a15c06d8cc1
Suar <albertosuar2004@gmail.com>
2025-04-12 00:01:23 +0200

    Modified Search widget

--- Sources/GUI/search.cpp
+++ Sources/GUI/search.cpp
@@ -1,29 +1,29 @@
 #include "../../Headers/GUI/search.h"
 #include "qboxlayout.h"
 
-Search::Search(QWidget* parent) : QWidget(parent), searchBar(new QLineEdit(this)), searchButton(new QPushButton(this)), advancedResearchButton(new QCheckBox("Ricerca Avanzata", this)), advancedSearch(new AdvancedResearch(this)), searchLayout(new QVBoxLayout(this)), simpleSearchLayout(new QHBoxLayout(this)) {
-
+Search::Search(QWidget* parent) : QWidget(parent), searchBar(new QLineEdit()), searchButton(new QPushButton()), advancedResearchButton(new QCheckBox("Ricerca Avanzata")), advancedSearch(new AdvancedResearch(this)), searchLayout(new QVBoxLayout()), simpleSearchLayout(new QHBoxLayout()) {   
+    //setStyleSheet("border : 2px solid blue; border-radius: 4px; ");
     searchBar->setPlaceholderText("Inserire titolo del contenuto...");
-    searchBar->setFixedSize(350, 40);
+    searchBar->setFixedSize(400, 40);
 
     searchButton->setFixedSize(40, 40);
     simpleSearchLayout->addWidget(searchBar);
     simpleSearchLayout->addWidget(searchButton);
     searchLayout->addLayout(simpleSearchLayout);
    
-    advancedResearchButton->setFixedSize(175, 20);
+    advancedResearchButton->setFixedSize(175, 15);
     searchLayout->addWidget(advancedResearchButton);
     
     advancedSearch->setVisible(false);
-    advancedSearch->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+    advancedSearch->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    //advancedSearch->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
     searchLayout->addWidget(advancedSearch);
 
-    searchLayout->setContentsMargins(5, 5, 5, 5);
+    searchLayout->setContentsMargins(15, 15, 15, 15);
     searchLayout->setAlignment(Qt::AlignLeft | Qt::AlignTop); 
     connect(advancedResearchButton, &QCheckBox::toggled, this, &Search::advancedResearch);
 
+    setLayout(searchLayout);
 }
 
-void Search::advancedResearch(bool checked) {
-    checked ? advancedSearch->show() : advancedSearch->hide();
-}
+void Search::advancedResearch(bool checked) { checked ? advancedSearch->show() : advancedSearch->hide(); }
