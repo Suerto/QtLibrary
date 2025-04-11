d20b369649b22a82ed0c8d38325e9ad307c0fe6a
Suar <albertosuar2004@gmail.com>
2025-04-12 00:18:27 +0200

    Added new Book filters

--- Sources/GUI/bookFilters.cpp
+++ Sources/GUI/bookFilters.cpp
@@ -1,24 +1,27 @@
 #include "../../Headers/GUI/bookFilters.h"
 
-BookFilters::BookFilters(QWidget* parent) : Filters(parent) {
-    //author->setPlaceholderText("Inserire nome dell'autore ");
-    author = new QLineEdit(this);
-    filtersLayout->addRow("Inserire Autore : ", author);
-    //editor->setPlaceholderText("Inserire editore del libro ");
-    editor = new QLineEdit(this);
-    filtersLayout->addRow("Inserire Editore : ", editor);
-
-    publisher = new QLineEdit(this);
-    filtersLayout->addRow("Inserire nome del Publisher : ", publisher);
+BookFilters::BookFilters(QWidget* parent) : Filters(parent), cover(new QComboBox(this)), pages(new QSpinBox(this)), author(new QLineEdit(this)), editor(new QLineEdit(this)), publisher(new QLineEdit(this)), genre(new QComboBox(this)) {
+    JsonHandler::loadEnumFromJson(cover, "Data/copertine.json", "Copertine");
     
-    genre = new QComboBox(this);
-    JsonHandler::loadEnumFromJson(genre, "copertine.json", "Copertine");
+    filtersLayout->addRow("Selezionare tipo di copertina : ", cover);
+
+    filtersLayout->addRow("Indicare numero di pagine : ", pages);
+    
+    filtersLayout->addRow("Inserire Autore : ", author);
+    
+    filtersLayout->addRow("Inserire Editore : ", editor);
+    
+    filtersLayout->addRow("Inserire nome del Publisher : ", publisher);
+      
+    JsonHandler::loadEnumFromJson(genre, "Data/generiLibri.json", "Generi Libri");
+    
     filtersLayout->addRow("Selezionare un genere : ", genre);
 }
 
 //gestita l'aggiunta delle possibili copertine che un libro può avere, occuparsi della rimozione del filtro selezionato per il refresh della pagina
 void BookFilters::reset() {
     Filters::reset(); //pensare ad una alternativa più performante
+    cover->setCurrentIndex(7);
     author->clear();
     editor->clear();
     publisher->clear();
