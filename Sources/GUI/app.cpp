50683e778d88f6663d9becd0c714195f02eb528a
Suar <albertosuar2004@gmail.com>
2025-04-13 00:21:19 +0200

    Modified loading json in app

--- Sources/GUI/app.cpp
+++ Sources/GUI/app.cpp
@@ -2,6 +2,9 @@
 #include "../../Headers/GUI/mainwindow.h"
 
 #include "../../Headers/LogicModel/libro.h"
+#include "../../Headers/LogicModel/manga.h"
+#include "../../Headers/LogicModel/film.h"
+#include "../../Headers/LogicModel/anime.h"
 
 App::App(int& argc, char** argv) : QApplication(argc, argv) {
     MainWindow* main = new MainWindow();
@@ -10,7 +13,17 @@ App::App(int& argc, char** argv) : QApplication(argc, argv) {
     JsonHandler::saveEnumToJson(Contenuto::getMappaLingue(), "Data/Dati.json" , "Lingue");
 
     JsonHandler::saveEnumToJson(Fisico::getMappaCopertine(), "Data/Dati.json", "Copertine");
+    
+    JsonHandler::saveEnumToJson(Periodico::getMappaCadenze(), "Data/Dati.json", "Cadenze");
+
+    JsonHandler::saveEnumToJson(Digitale::getMappaRisoluzioni(), "Data/Dati.json", "Risoluzioni");
 
     JsonHandler::saveEnumToJson(Libro::getMappaGeneri(), "Data/Dati.json", "Generi Libri");
+    
+    JsonHandler::saveEnumToJson(Manga::getMappaGeneri(), "Data/Dati.json", "Generi Manga");
+
+    JsonHandler::saveEnumToJson(Film::getMappaGeneri(), "Data/Dati.json", "Generi Film");
+    
+    JsonHandler::saveEnumToJson(Anime::getMappaGeneri(), "Data/Dati.json", "Generi Anime");
     main->show();
 }
