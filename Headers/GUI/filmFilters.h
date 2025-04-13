cc3d5823f735c8b15670392766c183c2dfdd0414
Suar <albertosuar2004@gmail.com>
2025-04-13 20:00:51 +0200

    Added new Film Filters

--- Headers/GUI/filmFilters.h
+++ Headers/GUI/filmFilters.h
@@ -3,12 +3,19 @@
 
 #include <QWidget>
 #include <QLineEdit>
+#include <QComboBox>
 
 #include "genericFilters.h"
 
 class FilmFilters : public Filters {
 private:
+    QComboBox* resolution;
+    QSpinBox* durata;
+    
     QLineEdit* director;
+    QLineEdit* composer;
+    QLineEdit* producer;
+    QComboBox* genre;
 public:
     FilmFilters(QWidget* parent = nullptr);
     void reset();
