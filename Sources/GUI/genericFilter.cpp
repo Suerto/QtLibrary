43d6472e837c8596cbcf39a45b62d6cc16ae4dbf
Suar <albertosuar2004@gmail.com>
2025-04-13 20:03:15 +0200

    Fixed a bug

--- Sources/GUI/genericFilter.cpp
+++ Sources/GUI/genericFilter.cpp
@@ -1,16 +1,13 @@
 #include "../../Headers/GUI/genericFilters.h"
-#include "qcombobox.h"
-#include "qformlayout.h"
 
 Filters::Filters(QWidget* parent) : QWidget(parent), language(new QComboBox(this)), year(new QSpinBox(this)), filtersLayout(new QFormLayout(this)) {
     filtersLayout->setSpacing(10);
 
     JsonHandler::loadEnumFromJson("Data/Dati.json", "Lingue", language);
-    language->setBaseSize(50, 20);
-    filtersLayout->addRow("Selezionare una lingua : ", language);
+    filtersLayout->addRow("Selezionare Lingua : ", language);
     
     year->setMaximum(2025);
-    filtersLayout->addRow("Indicare l'anno di pubblicazione : ", year);
+    filtersLayout->addRow("Anno di Pubblicazione : ", year);
 
     setLayout(filtersLayout);
 }
