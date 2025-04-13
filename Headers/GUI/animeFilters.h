ce6aecc5fec32f24b2f30e54b374f3cf2570ef8f
Suar <albertosuar2004@gmail.com>
2025-04-13 20:00:28 +0200

    Added new Anime Filters

--- Headers/GUI/animeFilters.h
+++ Headers/GUI/animeFilters.h
@@ -3,12 +3,22 @@
 
 #include <QWidget>
 #include <QLineEdit>
+#include <QComboBox>
+#include <QSpinBox>
+#include <QCheckBox>
 
 #include "genericFilters.h"
 
 class AnimeFilters : public Filters {
 private:
-    QLineEdit* composer;
+    QComboBox* resolution;
+    QSpinBox* durata;
+ 
+    QLineEdit* producer;
+    QSpinBox* episodes;
+    QSpinBox* seasons;
+    QCheckBox* subtitled;
+    QComboBox* genre;
 public:
     AnimeFilters(QWidget* parent = nullptr);
     void reset();
