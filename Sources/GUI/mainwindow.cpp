5e5703b329b8a5bc98ae7bf8543fcd63100697f9
Suar <albertosuar2004@gmail.com>
2025-04-15 10:42:48 +0200

    Added ToolBar and Creation

--- Sources/GUI/mainwindow.cpp
+++ Sources/GUI/mainwindow.cpp
@@ -1,8 +1,33 @@
 #include "../../Headers/GUI/mainwindow.h"
 
-MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), searchWidget(new Search()){
+MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), toolBar(new QToolBar(this)), windows(new QStackedWidget(this)) {
     resize(1200, 800);
+    
+    toolBar = addToolBar("Strumenti");
+    QPushButton* searching = new QPushButton("Ricerca", toolBar);
+    toolBar->addWidget(searching);
+    
+    QPushButton* creation = new QPushButton("Creazione", toolBar);
+    toolBar->addWidget(creation);
 
-    searchWidget->setFixedSize(width() / 2, height());
-    setCentralWidget(searchWidget);
+    windows->addWidget(new Search);
+    windows->addWidget(new Creation);
+     
+    setCentralWidget(windows);
+    windows->setVisible(false);
+
+    connect(searching, &QPushButton::clicked, this, &MainWindow::showSearching);
+    connect(creation, &QPushButton::clicked, this, &MainWindow::showAdvanced);
+}
+
+//Risolvere fattore di reset al variare del widget mostrato
+void MainWindow::showSearching() {
+   if(!windows->isVisible()) windows->setVisible(true);
+    
+   windows->setCurrentWidget(windows->widget(0));
+}
+
+void MainWindow::showAdvanced() {
+    if(!windows->isVisible()) windows->setVisible(true);
+    windows->setCurrentWidget(windows->widget(1));
 }
