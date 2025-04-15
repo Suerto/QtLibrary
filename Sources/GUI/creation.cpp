4a89a15caca3d7bb008e9fb048c7b2b3f1ad11aa
Suar <albertosuar2004@gmail.com>
2025-04-16 00:16:17 +0200

    Major update

--- Sources/GUI/creation.cpp
+++ Sources/GUI/creation.cpp
@@ -2,8 +2,12 @@
 
 Creation::Creation(QWidget* parent) : QWidget(parent), creationLayout(new QVBoxLayout(this)), title(new QLineEdit(this)) {
     title->setPlaceholderText("Inserire Titolo del Contenuto");
+    title->setFixedSize(300, 30);
     creationLayout->addWidget(title);
+    
 
 
+
+    creationLayout->setAlignment(Qt::AlignTop);
     setLayout(creationLayout);
 }
