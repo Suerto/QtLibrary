fd1b5285ed190af0be7cb798fadbc31bea8a2627
Suar <albertosuar2004@gmail.com>
2025-04-16 00:15:52 +0200

    New widgets added

--- Sources/GUI/search.cpp
+++ Sources/GUI/search.cpp
@@ -1,13 +1,14 @@
 #include "../../Headers/GUI/search.h"
 #include "qboxlayout.h"
 
-Search::Search(QWidget* parent) : QWidget(parent), searchBar(new QLineEdit()), searchButton(new QPushButton("Avviazionamentazione della Ricercazione")), advancedResearchButton(new QCheckBox("Ricerca Avanzata")), advancedSearch(new AdvancedResearch(this)), searchLayout(new QVBoxLayout()), simpleSearchLayout(new QHBoxLayout()) {   
+Search::Search(QWidget* parent) : QWidget(parent), searchBar(new QLineEdit()), searchButton(new QPushButton()), advancedResearchButton(new QCheckBox("Ricerca Avanzata")), advancedSearch(new AdvancedResearch(this)), searchLayout(new QVBoxLayout()), simpleSearchLayout(new QHBoxLayout()) {   
     searchBar->setPlaceholderText("Inserire titolo del contenuto...");
     searchBar->setFixedSize(400, 40);
 
     searchButton->setFixedSize(40, 40);
     simpleSearchLayout->addWidget(searchBar);
     simpleSearchLayout->addWidget(searchButton);
+    simpleSearchLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft);
     searchLayout->addLayout(simpleSearchLayout);
    
     advancedResearchButton->setFixedSize(175, 15);
@@ -17,11 +18,11 @@ Search::Search(QWidget* parent) : QWidget(parent), searchBar(new QLineEdit()), s
 
     searchLayout->addWidget(advancedSearch);
 
-    searchLayout->setContentsMargins(15, 15, 15, 15);
-    searchLayout->setAlignment(Qt::AlignLeft | Qt::AlignTop); 
+    //searchLayout->setContentsMargins(15, 15, 15, 15);
+    searchLayout->setAlignment(Qt::AlignTop | Qt::AlignLeft); 
     connect(advancedResearchButton, &QCheckBox::toggled, this, &Search::advancedResearch);
 
     setLayout(searchLayout);
 }
 
-void Search::advancedResearch(bool checked) { checked ? advancedSearch->show() : advancedSearch->hide(); }
+void Search::advancedResearch(bool checked) { checked ? advancedSearch->setVisible(true) : advancedSearch->setVisible(false); }
