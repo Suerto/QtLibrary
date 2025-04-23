13c1a9f1848577bf4f921f7f54a8d122401d74cc
Suar <albertosuar2004@gmail.com>
2025-04-23 09:08:43 +0200

    Improved error logic when changing on creation

--- Sources/GUI/creation.cpp
+++ Sources/GUI/creation.cpp
@@ -1,36 +1,32 @@
 #include "../../Headers/GUI/creation.h"
-#include "qbuttongroup.h"
-#include "qnamespace.h"
-#include "qobject.h"
 
-Creation::Creation(QWidget* parent) : QWidget(parent), creationLayout(new QVBoxLayout(this)), reset(new QPushButton("Riprista filtri", this)), title(new QLineEdit(this)), buttonLayout(new QHBoxLayout), type(new QButtonGroup(this)), typeForm(new QStackedWidget(this)), choice(new QPushButton("Crea Contenuto", this)) {
-    
-    
+Creation::Creation(QWidget* parent) : QWidget(parent), creationLayout(new QVBoxLayout(this)), reset(new QPushButton("Riprista filtri", this)), title(new QLineEdit(this)), buttonLayout(new QHBoxLayout), type(new QButtonGroup(this)), typeForm(new QStackedWidget(this)), choice(new QPushButton("Crea Contenuto", this)) {  
     creationLayout->addWidget(reset, Qt::AlignLeft);
 
     title->setPlaceholderText("Inserire Titolo del Contenuto");
+    title->font();
     title->setFixedSize(300, 30);
     creationLayout->addWidget(title, Qt::AlignLeft | Qt::AlignTop);
     
     QPushButton* book = new QPushButton("Libro", this);
     book->setFixedSize(100, 25);
     book->setCheckable(true);
-    type->addButton(book, 0);
+    type->addButton(book, 1);
 
     QPushButton* manga = new QPushButton("Manga", this);
     manga->setFixedSize(100, 25);
     manga->setCheckable(true);
-    type->addButton(manga, 1);
+    type->addButton(manga, 2);
 
     QPushButton* film = new QPushButton("Film", this);
     film->setFixedSize(100, 25);
     film->setCheckable(true);
-    type->addButton(film, 2);
+    type->addButton(film, 3);
 
     QPushButton* anime = new QPushButton("Anime", this);
     anime->setFixedSize(100, 25);
     anime->setCheckable(true);
-    type->addButton(anime, 3);
+    type->addButton(anime, 4);
 
     foreach(QAbstractButton* button, type->buttons()) buttonLayout->addWidget(button);
 
@@ -43,35 +39,42 @@ Creation::Creation(QWidget* parent) : QWidget(parent), creationLayout(new QVBoxL
     typeForm->insertWidget(4, new AnimeFilters);
 
     typeForm->setVisible(false);
+    //typeForm->setCurrentIndex(-1);
     creationLayout->addWidget(typeForm);
     creationLayout->setAlignment(Qt::AlignCenter);
 
     creationLayout->addWidget(choice); 
 
     connect(type, &QButtonGroup::idClicked, this, &Creation::showTypeForm);
-
     connect(reset, &QPushButton::clicked, this, &Creation::resetCreation);
-
     connect(choice, &QPushButton::clicked, this, &Creation::startCreation);
 
 }
 
 void Creation::showTypeForm(int id) {
     //gestione del tentativo di cambio del widget durante la creazione dell'oggetto
-    if(typeForm->currentIndex() != id && typeForm->currentIndex() != -1) {
-        ErrorDialog* change = new ErrorDialog(this);
-        connect(change, &ErrorDialog::azione, this, [this, id](const QString& choice) {
+    if(typeForm->currentIndex() != 0) {
+        if(typeForm->currentIndex() != id) {
+            ErrorDialog* change = new ErrorDialog(this);
+            connect(change, &ErrorDialog::azione, this, [this, id](const QString& choice) {
                 if(choice == "Conferma") {
                     qobject_cast<Filters*>(typeForm->currentWidget())->reset();
+                    typeForm->setCurrentIndex(id);
                 }
-        });
+                else {
+                    type->button(typeForm->currentIndex())->click();
+                }
+            });
 
-        change->exec();
+            change->exec();
+        }
+        else return;
     }
 
-    typeForm->setCurrentIndex(id);
-    typeForm->setVisible(true);
-
+    else {
+        typeForm->setCurrentIndex(id);
+        typeForm->setVisible(true);
+    }
 }
 
 void Creation::resetCreation() {
@@ -83,4 +86,5 @@ void Creation::startCreation() {
    QVariantMap parameters = qobject_cast<Filters*>(typeForm->currentWidget())->raccogliDati();
 
    parameters["titolo"] = title->text();
+
 }
