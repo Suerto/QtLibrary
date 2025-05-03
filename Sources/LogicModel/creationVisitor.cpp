b304111272625d0ae8fee88b2a8bb54b4e8b504d
Suar <albertosuar2004@gmail.com>
2025-05-03 21:43:42 +0200

    Removed 'Undefined' check because duplicated from raccogliDati()

--- Sources/LogicModel/creationVisitor.cpp
+++ Sources/LogicModel/creationVisitor.cpp
@@ -1,258 +1,153 @@
 #include "../../Headers/LogicModel/creationVisitor.h"
 
+//La Creazione di un contenuto non può permettere in alcn modo che l'utnete possa non definire un attributo dell'oggetto
 CreationVisitor::CreationVisitor(const unordered_map<string, string>& prmts) : parametri(prmts) {}
 
 void CreationVisitor::visit(Libro& lib) const {
     // Anno
     auto it = parametri.find("Anno");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        lib.setAnno(0);  // Default: Anno "0"
-
+    lib.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
+   
     // Lingua
     it = parametri.find("Lingua");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setLingua(it->second);
-    else
-        lib.setLingua("Indefinita");
-
+    lib.setLingua(it->second);
     // Copertina
     it = parametri.find("Copertina");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setCopertina(it->second);
-    else
-        lib.setCopertina("Indefinita");
+    lib.setCopertina(it->second);
 
     // Pagine
     it = parametri.find("Pagine");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setPagine(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        lib.setPagine(0);  // Default: Pagine "0"
+    lib.setPagine(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Autore
     it = parametri.find("Autore");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setAutore(it->second);
-    else
-        lib.setAutore("Indefinito");
+    lib.setAutore(it->second);
 
     // Editore
     it = parametri.find("Editore");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setEditore(it->second);
-    else
-        lib.setEditore("Indefinito");
+    lib.setEditore(it->second);
 
     // Publisher
     it = parametri.find("Publisher");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setPublisher(it->second);
-    else
-        lib.setPublisher("Indefinito");
+    lib.setPublisher(it->second);
 
     // Genere
     it = parametri.find("Genere");
-    if(it != parametri.end() && !it->second.empty())
-        lib.setGenere(it->second);
-    else
-        lib.setGenere("Indefinito");
+    lib.setGenere(it->second);
 }
 
 void CreationVisitor::visit(Manga& mng) const {
     // Anno
     auto it = parametri.find("Anno");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        mng.setAnno(0);  // Default: Anno "0"
+    mng.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Lingua
     it = parametri.find("Lingua");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setLingua(it->second);
-    else
-        mng.setLingua("Indefinita");
+    mng.setLingua(it->second);
 
     // Copertina
     it = parametri.find("Copertina");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setCopertina(it->second);
-    else
-        mng.setCopertina("Indefinita");
+    mng.setCopertina(it->second);
 
     // Pagine
     it = parametri.find("Pagine");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setPagine(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        mng.setPagine(0);  // Default: Pagine "0"
+    mng.setPagine(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Cadenza
     it = parametri.find("Cadenza");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setCadenza(it->second);
-    else
-        mng.setCadenza("Indefinita");
+    mng.setCadenza(it->second);
 
     // Mangaka
     it = parametri.find("Mangaka");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setMangaka(it->second);
-    else
-        mng.setMangaka("Indefinito");
+    mng.setMangaka(it->second);
 
     // Editore
     it = parametri.find("Editore");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setEditore(it->second);
-    else
-        mng.setEditore("Indefinito");
+    mng.setEditore(it->second);
 
     // Capitoli
     it = parametri.find("Capitoli");
-    if(it != parametri.end() && !it->second.empty())
-        mng.setCapitoli(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        mng.setCapitoli(0);  // Default: Capitoli "0"
+    mng.setCapitoli(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Genere
     it = parametri.find("Genere");
-    if(it != parametri.end() && !it->second.empty())
         mng.setGenere(it->second);
-    else
-        mng.setGenere("Indefinito");
 }
 
 void CreationVisitor::visit(Film& flm) const {
     // Anno
     auto it = parametri.find("Anno");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        flm.setAnno(0);  // Default: Anno "0"
+    flm.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Lingua
     it = parametri.find("Lingua");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setLingua(it->second);
-    else
-        flm.setLingua("Indefinita");
+    flm.setLingua(it->second);
 
     // Risoluzione
     it = parametri.find("Risoluzione");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setRisoluzione(it->second);
-    else
-        flm.setRisoluzione("Indefinita");
+    flm.setRisoluzione(it->second);
 
     // Durata
     it = parametri.find("Durata");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setDurata(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        flm.setDurata(0);  // Default: Durata "0"
+    flm.setDurata(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Regista
     it = parametri.find("Regista");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setRegista(it->second);
-    else
-        flm.setRegista("Indefinito");
+    flm.setRegista(it->second);
 
     // Compositore
     it = parametri.find("Compositore");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setComposer(it->second);
-    else
-        flm.setComposer("Indefinito");
+    flm.setComposer(it->second);
 
     // Produttore
     it = parametri.find("Produttore");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setProducer(it->second);
-    else
-        flm.setProducer("Indefinito");
+    flm.setProducer(it->second);
 
     // Genere
     it = parametri.find("Genere");
-    if(it != parametri.end() && !it->second.empty())
-        flm.setGenere(it->second);
-    else
-        flm.setGenere("Indefinito");
+    flm.setGenere(it->second);
 }
 
 void CreationVisitor::visit(Anime& anm) const {
     // Anno
     auto it = parametri.find("Anno");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        anm.setAnno(0);  // Default: Anno "0"
+    anm.setAnno(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Lingua
     it = parametri.find("Lingua");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setLingua(it->second);
-    else
-        anm.setLingua("Indefinita");
+    anm.setLingua(it->second);
 
     // Risoluzione
     it = parametri.find("Risoluzione");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setRisoluzione(it->second);
-    else
-        anm.setRisoluzione("Indefinita");
+    anm.setRisoluzione(it->second);
 
     // Durata
     it = parametri.find("Durata");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setDurata(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        anm.setDurata(0);  // Default: Durata "0"
+    anm.setDurata(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Cadenza
     it = parametri.find("Cadenza");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setCadenza(it->second);
-    else
-        anm.setCadenza("Indefinita");
+    anm.setCadenza(it->second);
 
     // Episodi
     it = parametri.find("Episodi");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setEpisodi(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        anm.setEpisodi(0);  // Default: Episodi "0"
+    anm.setEpisodi(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Stagioni
     it = parametri.find("Stagioni");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setStagioni(static_cast<unsigned int>(std::stoi(it->second)));
-    else
-        anm.setStagioni(0);  // Default: Stagioni "0"
+    anm.setStagioni(static_cast<unsigned int>(std::stoi(it->second)));
 
     // Sottotitolato
     it = parametri.find("Sottotitolato");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setSubtitle(static_cast<bool>(std::stoi(it->second)));
-    else
-        anm.setSubtitle(false);  // Default: Non sottotitolato
+    anm.setSubtitle(static_cast<bool>(std::stoi(it->second)));
 
     // Produttore
     it = parametri.find("Produttore");
-    if(it != parametri.end() && !it->second.empty())
-        anm.setProducer(it->second);
-    else
-        anm.setProducer("Indefinito");
+    anm.setProducer(it->second);
 
     // Genere
     it = parametri.find("Genere");
-    if(it != parametri.end() && !it->second.empty())
         anm.setGenere(it->second);
-    else
-        anm.setGenere("Indefinito");
 }
 
 unordered_map<string, string> CreationVisitor::getMap() const { return parametri; }
