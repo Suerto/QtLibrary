19e66cb78a9adcea53cd179780fbcd855183a39b
Suar <albertosuar2004@gmail.com>
2025-05-12 22:27:54 +0200

    Added elimination and modifcation logic

--- Headers/GUI/contentViewer.h
+++ Headers/GUI/contentViewer.h
@@ -9,14 +9,19 @@
 #include <QHBoxLayout>
 
 #include "genericFilters.h"
+#include "../LogicModel/creationVisitor.h"
+#include "../LogicModel/contenuto.h"
+
 
 class ContentViewer : public QWidget {
     Q_OBJECT 
 private:
+    Contenuto* object;
+
     QLineEdit* title;
     QLineEdit* type;
     Filters* dettagli;
-
+    
     QPushButton* modify;
     QPushButton* remove;
     QPushButton* save;
@@ -25,20 +30,22 @@ private:
     QVBoxLayout* contentLayout;
     QHBoxLayout* buttonsLayout;
 public:
-    ContentViewer(const QString& ttl, const QString& tp, Filters* flts, QWidget* parent = nullptr);
+    ContentViewer(const QString& ttl, const QString& tp, Filters* flts, Contenuto* obj, QWidget* parent = nullptr);
     void abilitaPulsantiReadOnly(const bool& rom);
 
     void pulsantiModificaAttivi(const bool& rom);
 
     void restoreFilter(const std::unordered_map<string, string>& attributes);
+
+    Contenuto* getObject() const;
+    ~ContentViewer();
 public slots:
-    void modifica();
     void rimuovi();
+    void modifica();
 signals:
     void modificaAvviata(ContentViewer* contenuto);
     void modificaAnnullata();
     void rimuoviContenuto(ContentViewer* contenuto);
-    
-    ~ContentViewer();
+    void modificaContenuto(const unordered_map<string, string>& modifiche);
 };
 #endif //CONTENT_VIEWER_H
--- Sources/GUI/contentViewer.cpp
+++ Sources/GUI/contentViewer.cpp
@@ -1,6 +1,7 @@
 #include "../../Headers/GUI/contentViewer.h"
+#include "qglobal.h"
 
-ContentViewer::ContentViewer(const QString& ttl, const QString& tp, Filters* flts, QWidget* parent) : QWidget(parent), title(new QLineEdit(this)), type(new QLineEdit(this)), dettagli(flts), modify(new QPushButton("Modifica", this)), remove(new QPushButton("Elimina", this)), save(new QPushButton("Salva", this)), cancel(new QPushButton("Annulla", this)), contentLayout(new QVBoxLayout(this)), buttonsLayout(new QHBoxLayout(this)) {
+ContentViewer::ContentViewer(const QString& ttl, const QString& tp, Filters* flts, Contenuto* obj, QWidget* parent) : QWidget(parent),  object(obj), title(new QLineEdit(this)), type(new QLineEdit(this)), dettagli(flts), modify(new QPushButton("Modifica", this)), remove(new QPushButton("Elimina", this)), save(new QPushButton("Salva", this)), cancel(new QPushButton("Annulla", this)), contentLayout(new QVBoxLayout()), buttonsLayout(new QHBoxLayout()) {
     title->setText(ttl);
     title->setAlignment(Qt::AlignCenter);
     title->setReadOnly(true);
@@ -51,22 +52,38 @@ void ContentViewer::abilitaPulsantiReadOnly(const bool& rom) {
 void ContentViewer::modifica() {
     emit modificaAvviata(this);
     
+    qDebug() << QString::fromStdString("Siamo nella modifica");
     std::unordered_map<string, string> tmp = dettagli->raccogliDati();
 
     dettagli->setModifiable(true);
     
     abilitaPulsantiReadOnly(false);
     connect(cancel, &QPushButton::clicked, this, [tmp, this]() {
+        dettagli->setModifiable(false);
 
-            dettagli->setModifiable(false);
+        emit modificaAnnullata();
+        dettagli->setModifiable(false);
+        abilitaPulsantiReadOnly(true);
+        restoreFilter(tmp);
+        });
+    
+    connect(save, &QPushButton::clicked, this, [this]() {
+            qDebug() << "Siamo nella connect del save";
+            unordered_map<string, string> modifiche = dettagli->raccogliDati();
+            modifiche.insert({"Titolo", title->text().toStdString()});
+            CreationVisitor visitor(modifiche);
+            qDebug() << "Abbiamo raccolto i dati"; 
 
-            emit modificaAnnullata();
+            for(const auto&[T, V] : dettagli->raccogliDati()) {
+                qDebug() << QString::fromStdString(T) << " " << QString::fromStdString(V); 
+            }
+            qDebug() << "Indirizzo oggetto : " << static_cast<void*>(object);
+            object->accept(&visitor);
             dettagli->setModifiable(false);
             abilitaPulsantiReadOnly(true);
-            restoreFilter(tmp);
-            });
+            restoreFilter(modifiche);
+        });
     }
-
 void ContentViewer::pulsantiModificaAttivi(const bool& rom) {
     remove->setVisible(rom);
     modify->setVisible(rom);
@@ -75,3 +92,5 @@ void ContentViewer::pulsantiModificaAttivi(const bool& rom) {
 ContentViewer::~ContentViewer() {
     delete dettagli;
 }
+
+Contenuto* ContentViewer::getObject() const { return object; }
