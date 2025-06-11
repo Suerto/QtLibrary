65e8059985183e215f7000c49836f4a6c7130039
Alberto Suar <albertosuar2004@gmail.com>
2025-06-11 18:51:12 +0200

    Delete consegna directory

--- consegna/QtLibrary/Sources/GUI/duplicateVerifier.cpp
+++
@@ -1,8 +0,0 @@
-#include "../../Headers/GUI/duplicateVerifier.h"
-#include <QDebug>
-
-DuplicateVerifier::DuplicateVerifier(ContentManager* mngr) : manager(mngr) {}
-
-bool DuplicateVerifier::isThereADuplicate(const int& index, const unordered_map<string, string>& map) const {
-    return manager->controllaDuplicato(index, map);
-}
--- consegna/QtLibrary/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Headers/GUI/app.h"
-
-int main(int argc, char** argv) {
-    qputenv("QT_QPA_PLATFORM", "xcb");
-    ContentManager appManager;
-    App app(argc, argv, &appManager);
-    return app.exec();
-}
